c20346cc048abaa1484c1e1673c782e6c69cc799
Timmy <timisutantyo@gmail.com>
2024-05-05 14:16:28 +0200

    Learning about comment

---
+++ demo/comment.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    /* This echo shows text.
+
+     If not commented, text will not be grayed out
+
+     this is the second link */
+
+     // one line comment
+
+     // code can also be commented if doesn't want to be executed.
+
+    echo "Hello Student";
+    ?>
+</body>
+</html>
