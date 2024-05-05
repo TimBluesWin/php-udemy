0cff7b73297632f9b6ef75ec0cb591e687acff1e
Timmy <timisutantyo@gmail.com>
2024-05-05 09:06:05 +0200

    Initial commit

---
+++ demo/embed.php
@@ -0,0 +1,12 @@
+<?php 
+// php code here; (note: after semicolon can be another line of code, but for me that will make code
+// unreadable.)
+
+// php code here;
+
+// Note: there are shorthand versions such as <? \?\>, <% %>, and <?= \?\>, but it's not recommended because
+// not all servers accept it.
+
+echo "hello student";
+?>
+
