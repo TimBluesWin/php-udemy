44f613eba7ac5c1dd5848fe46e50066b26863921
Timmy <timisutantyo@gmail.com>
2024-05-29 05:11:22 +0200

    Add file for if statement

---
+++ demo/ifstatement.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        // if(3 < 10) {
+        //     echo "three is less than ten. <br>";
+        // }
+        if(3 > 10) {
+            // Nothing happens
+            echo "three is less than ten. <br>";
+        }
+        elseif(4 > 5){
+            echo "of course, four is less than five!";
+
+        }
+        else {
+            echo "it is not!!";
+        }
+    ?>
+</body>
+</html>
