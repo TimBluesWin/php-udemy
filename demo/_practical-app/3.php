a599bb5bb54a36f430f8cffba4b59e0c70443a28
Timmy <timisutantyo@gmail.com>
2024-05-30 13:32:23 +0200

    Completing exercise 3

--- demo/_practical-app/3.php
+++ demo/_practical-app/3.php
@@ -16,7 +16,7 @@
 
 /*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
 
-
+	
 
 	Step 2: Make a forloop  that displays 10 numbers
 
@@ -25,7 +25,42 @@
 
  */
 
-	
+ if(3 > 4)
+ {
+	echo "No, it's not true! <br>";
+ }
+ else if(false)
+ {
+	echo "This is false again! <br>";
+ }
+ else
+ {
+	echo "I love PHP <br>";
+ }
+ for($i = 0; $i < 10; $i++)
+ {
+	echo $i . "<br>";
+ }
+ $variable = 85;
+ switch($variable){
+	case $variable == 100:
+		echo "perfect!";
+		break;
+	case $variable > 80 and $variable < 100:
+		echo "good";
+		break;
+	case $variable > 60 and $variable < 80:
+		echo "okay";
+		break;
+	case $variable > 40 and $variable < 60:
+		echo "bad";
+		break;
+	case $variable > 20 and $variable < 40:
+		echo "horrible";
+		break;
+	default:
+		echo "value invalid";
+ }
 ?>
 
 
