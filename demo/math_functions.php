6103ffc3d85ce59e6c4bf98b15e9fe7a33623be9
Timmy <timisutantyo@gmail.com>
2024-06-02 13:24:48 +0200

    Math functions in PHP

---
+++ demo/math_functions.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo pow(2, 7); // 2^7
+
+    echo "<br>";
+
+    echo rand(1, 1000); // random number from 1 to 1000
+
+    echo "<br>";
+
+    echo sqrt(100); //square root of 100
+
+    echo "<br>";
+
+    echo ceil(4.6); //rounds up 4.6 to 5
+
+    echo "<br>";
+
+    echo floor(4.6); //rounds down 4.6 to 4
+
+    echo "<br>";
+
+    echo round(4.6); //rounds 4.6 to 5
+    ?>
+</body>
+</html>
