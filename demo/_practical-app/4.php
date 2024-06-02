a7f542e80b4be22ac4e1e2097f6de5cb4fbca55c
Timmy <timisutantyo@gmail.com>
2024-06-02 13:27:26 +0200

    Doing exercise 4

--- demo/_practical-app/4.php
+++ demo/_practical-app/4.php
@@ -15,7 +15,12 @@
 
 	
 	<?php  
+	function multiply_numbers($number_1, $number_2)
+	{
+		return $number_1 * $number_2;
+	}
 
+	echo multiply_numbers(5, 4);
 /*  Step1: Define a function and make it return a calculation of 2 numbers
 
 	Step 2: Make a function that passes parameters and call it using parameter values
