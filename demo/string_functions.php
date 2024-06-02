a57403986d249e45f9e4b57437a2c90a996e71cd
Timmy <timisutantyo@gmail.com>
2024-06-02 13:27:15 +0200

    Learning about string functions

---
+++ demo/string_functions.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $string = "Hello student do you like the class";
+
+    echo strlen($string); // length of string
+
+    echo "<br>";
+
+    echo strtoupper($string); // make string uppercase 
+
+    echo "<br>";
+
+    echo strtolower($string); // make string lowercase 
+    ?>
+</body>
+</html>
